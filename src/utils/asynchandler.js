14d4d7b9359cd4d99344e1fef7f6ebb965334188
Siddharth <siddharthdeol18@gmail.com>
2025-07-24 22:27:47 +0530

    utils code

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,20 @@
+const asynhandler=(requesthandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requesthandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+
+
+export {asynhandler}
+
+//const asynhandler=(fn)=>async(req,res,next) => {
+  //  try{
+  //      await fn(req,res,next)
+ //   }catch(error){
+ //       res.status(error.code || 500).json({
+ //           success:false,
+ //           message:error.message
+ //       })
+  //  }
+//}
